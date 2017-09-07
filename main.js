3c680e28106558c00c6ace4127613f7d02f07d3d
rayGreycloud <rayGreycloud@gmail.com>
2017-09-06 18:36:28 -0700

    Refactor using delay function

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 anime({
-  targets: 'div.box.red',
+  targets: 'div.box',
   translateY: [
     { value: 200, duration: 500 },
     { value: 0, duration: 800 }
@@ -7,44 +7,57 @@ anime({
   rotate: {
     value: '1turn',
     easing: 'easeInOutSine'
-  }
+  },
+  delay: function(el, i, l) { return i * 1000 }
 });
 
-anime({
-  targets: 'div.box.blue',
-  translateY: [
-    { value: 200, duration: 500, delay: 1000 },
-    { value: 0, duration: 800 }
-  ],
-  rotate: {
-    value: '1turn',
-    easing: 'easeInOutSine',
-    delay: 1000
-  }
-});
-
-anime({
-  targets: 'div.box.green',
-  translateY: [
-    { value: 200, duration: 500, delay: 2000 },
-    { value: 0, duration: 800 }
-  ],
-  rotate: {
-    value: '1turn',
-    easing: 'easeInOutSine',
-    delay: 2000
-  }
-});
-
-anime({
-  targets: 'div.box.yellow',
-  translateY: [
-    { value: 200, duration: 500, delay: 3000 },
-    { value: 0, duration: 800 }
-  ],
-  rotate: {
-    value: '1turn',
-    easing: 'easeInOutSine',
-    delay: 3000
-  }
-});
+// anime({
+//   targets: 'div.box.red',
+//   translateY: [
+//     { value: 200, duration: 500 },
+//     { value: 0, duration: 800 }
+//   ],
+//   rotate: {
+//     value: '1turn',
+//     easing: 'easeInOutSine'
+//   }
+// });
+//
+// anime({
+//   targets: 'div.box.blue',
+//   translateY: [
+//     { value: 200, duration: 500, delay: 1000 },
+//     { value: 0, duration: 800 }
+//   ],
+//   rotate: {
+//     value: '1turn',
+//     easing: 'easeInOutSine',
+//     delay: 1000
+//   }
+// });
+//
+// anime({
+//   targets: 'div.box.green',
+//   translateY: [
+//     { value: 200, duration: 500, delay: 2000 },
+//     { value: 0, duration: 800 }
+//   ],
+//   rotate: {
+//     value: '1turn',
+//     easing: 'easeInOutSine',
+//     delay: 2000
+//   }
+// });
+//
+// anime({
+//   targets: 'div.box.yellow',
+//   translateY: [
+//     { value: 200, duration: 500, delay: 3000 },
+//     { value: 0, duration: 800 }
+//   ],
+//   rotate: {
+//     value: '1turn',
+//     easing: 'easeInOutSine',
+//     delay: 3000
+//   }
+// });
